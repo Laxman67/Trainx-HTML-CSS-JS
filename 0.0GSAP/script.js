d74e599a5b571bef97618ecb2b17b0ad88d540eb
Laxman67 <laxman.webdev67@gmail.com>
2026-05-27 18:11:05 +0530

    added all files and folders

---
+++ 0.0GSAP/script.js
@@ -0,0 +1,14 @@
+gsap.from('#page1 #box', { scale: 0, rotate: 360, duration: 1 });
+gsap.from('#page2 #box', {
+  scale: 0,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: '#page2 #box',
+    scroller: 'body',
+    markers: true,
+    start: 'top 60%',
+    end: 'top 30%',
+    scrub: 3,
+    pin: true,
+  },
+});
---
+++ 0.1DM/script.js
@@ -0,0 +1,15 @@
+//Primitive Data Types
+let employee = {
+  name: 'Dheeraj',
+  age: 21, //
+  address: {
+    city: 'Vadodra',
+    pincode: 125670,
+  },
+  email: 'dheeraj@gmail.com',
+};
+
+//Optional chaining ?.
+// Nulling Coleasing Operator ??
+
+console.log('Hello' ?? 'No data available ');
