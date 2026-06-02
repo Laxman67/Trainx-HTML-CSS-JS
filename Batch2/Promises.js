8a27a858e6789fb93c07cc4b314d1d4472c36ca5
Laxman67 <laxman.webdev67@gmail.com>
2026-06-02 18:04:14 +0530

    added content in Batch2

--- Batch2/Promises.js
+++ Batch2/Promises.js
@@ -1,5 +1,15 @@
-// debugger;
-let name = 'laksh';
-let age = undefined;
+async function getData() {
+  console.log('getData() Start'); //1
 
-let arr = [12, 34, 56, 78, 12];
+  try {
+    let data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+
+    console.log('getData() End'); //2
+  } catch (error) {
+    console.log(`Error Message :${error.message}`);
+  }
+}
+
+getData();
