0cd02e59fc0d660b58c3f3e4526f4b84f4ef91ad
Laksh67 <laxman.webdev67@gmail.com>
2026-08-09 22:40:03 +0530

    studionamma top section

--- 16.DOM/Project/5.Studionamma/script.js
+++ 16.DOM/Project/5.Studionamma/script.js
@@ -1,40 +1,12 @@
-let animatedBox = document.getElementById('animated-char-box');
+let cursorBox = document.getElementById('animated-cursor-box');
+let topSection = document.getElementById('top-section');
 
-let letters = animatedBox.querySelectorAll('p');
-
-letters.forEach((letter) => {
-  let x = (Math.random() - 0.5) * 300;
-  let y = (Math.random() - 0.5) * 300;
-
-  let rotate = (Math.random() - 0.5) * 60;
-
-  let scale = Math.random() * 0.5 + 0.7;
-
-  letter.style.setProperty('--x', `${x}px`);
-  letter.style.setProperty('--y', `${y}px`);
-  letter.style.setProperty('--rotate', `${rotate}deg`);
-  letter.style.setProperty('--scale', scale);
+topSection.addEventListener('mousemove', (e) => {
+  cursorBox.style.opacity = 1;
+  cursorBox.style.left = `${e.clientX}px`;
+  cursorBox.style.top = `${e.clientY}px`;
 });
 
-setTimeout(() => {
-  animatedBox.classList.add('animate');
-}, 6500);
-
-// Smooth trailing cursor box
-let cursorBox = document.getElementById('animated-box-cursor-box');
-let animatedCharBox = document.getElementById('animated-char-box');
-let container = animatedCharBox.parentElement; // .top-section
-let topSection = document.querySelector('.top-section');
-
-// topSection.addEventListener('mouseover', (e) => {
-//   const { offsetX, offsetY } = e;
-//   cursorBox.style.cssText = `
-//  display:block;
-//  left:${e.clientX}px;
-//  top:${offsetY}px;
-//  transition:
-//           left 0.3s ease-in-out,
-//           top 0.3s ease-in-out;
-
-//   `;
-// });
+topSection.addEventListener('mouseleave', (e) => {
+  cursorBox.style.opacity = 0;
+});
